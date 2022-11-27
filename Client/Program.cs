f068a0b9da5cf1e1635489fee3a06f8db182209d
Marcos Carvalho <marcoscarvalhop@outlook.com>
2022-11-27 14:26:09 -0300

    Att de Domingo

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,3 +1,6 @@
+global using BlazorEcommerce.Shared;
+global using System.Net.Http.Json;
+global using BlazorEcommerce.Client.Services.ProductService;
 using BlazorEcommerce.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
@@ -5,6 +8,8 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped<IProductService, ProductService>();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
